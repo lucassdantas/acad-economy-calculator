f40a573b32d249f294ae88c6af64fb8f8098530f
Lucas Dantas <lucasdantasprogramador@gmail.com>
2025-03-30 08:13:21 -0300

    add check icon

--- src/app/components/Form/ProgressItems/index.tsx
+++ src/app/components/Form/ProgressItems/index.tsx
@@ -1,5 +1,6 @@
 import { ProgressBar, ProgressContainer } from '@/app/components/Form/ProgressItems/ProgressBar';
 import React from 'react'
+import { FaRegCheckCircle } from 'react-icons/fa';
 
 interface ProgressBarProps{
   currentStep:number;
@@ -13,7 +14,7 @@ export const ProgressItems = ({currentStep, currentStepProgress}:ProgressBarProp
         <div key={step} className=''>
           <div className={`relative flex justify-center items-center bg-white rounded-full p-2 shadow-[inset_0px_0px_2px_#1D70B780] `}>
             <span className={`${currentStep >= step? 'bg-acad-blue text-white':' text-acad-gray-dark'} flex items-center justify-center rounded-full border border-acad-blue text-center font-bold text-xl w-[36px] h-[36px] z-40`} >
-              {step}
+              {currentStep>step? <FaRegCheckCircle className='text-acad-yellow' />:step}
             </span>
           </div>
           {step<steps.length && <ProgressContainer left={step===1? 12:69}/>}
