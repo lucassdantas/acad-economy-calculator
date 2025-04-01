bb4070b3bcbf67e8028703274032e390b1316e20
Lucas Dantas <lucasdantasprogramador@gmail.com>
2025-03-31 21:43:52 -0300

    add image to first screen

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -27,15 +27,15 @@ export default function Home() {
 
 const BeforeInitApp = ({isAppStarted, setIsAppStarted}:{isAppStarted:boolean, setIsAppStarted:(isAppStarted:boolean)=>void}) => {
   return(
-    <div className='w-full h-fit text-center flex flex-col items-center gap-4 md:flex-row'>
+    <div className='w-full h-fit text-center flex flex-col items-center gap-4 md:flex-row relative'>
       <BlueForm className='absolute bottom-0 right-0'/>
       <div className='w-full text-left md:flex md:flex-col md:items-start md:justify-between md:h-screen md:w-[50%]'>
-        <img src={'/imgs/discount-man.png'} alt='Homem com ícones de desconto' className='w-[200px] block md:hidden'/>
+        <img src={'/imgs/discount-man.png'} alt='Homem com ícones de desconto' className='w-full mt-58 block md:hidden'/>
         <img src={'/imgs/discount-man-vertical.png'} alt='Homem com ícones de desconto' className='hidden md:block  w-full object-contain max-w-[752px]  text-left'/>
       </div>
-      <div className='flex flex-col items-center justify-center md:justify-start md:text-start md:w-[50%] gap-4'>
+      <div className='flex flex-col items-center justify-center md:justify-start md:text-start md:w-[50%] gap-4 mb-24 md:mb-0 px-4'>
         <Title tag='h1'>Descubra quanto sua academia pode economizar</Title>
-        <Title tag='h2' className='text-acad-gray-dark'>sendo um associado ACAD Brasil!</Title>
+        <Title tag='h2' className='text-acad-gray-dark mb-4'>sendo um associado ACAD Brasil!</Title>
         <div className='w-full flex justify-center md:justify-start md:mt-12'>
           <Button onClick={() => setIsAppStarted(true)}>Calcular minha economia agora</Button>
         </div>
