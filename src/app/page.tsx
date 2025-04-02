173987ec3167ee769ee6232e7017bb9340026b93
Lucas Dantas <lucasdantasprogramador@gmail.com>
2025-04-02 19:41:30 -0300

    fix responsivity image of first screen on mobile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -27,17 +27,22 @@ export default function Home() {
 
 const BeforeInitApp = ({isAppStarted, setIsAppStarted}:{isAppStarted:boolean, setIsAppStarted:(isAppStarted:boolean)=>void}) => {
   return(
-    <div className='w-full min-h-screen text-center flex flex-col justify-start items-center gap-4 md:flex-row relative'>
+    <div className='w-full min-h-screen text-center flex flex-col lg:flex-row items-center gap-4  relative overflow-y-hidden'>
       <BlueAndYellowForms className=''/>
-      <div className='w-full md:w-[50%] text-center flex flex-col items-center justify-center md:items-center md:justify-center pt-12'>
-        <img src='/imgs/acad-logo.png' alt='Acad Logotipo' className='max-w-[300px] -mb-4 '/>
-        <img src={'/imgs/discount-man.png'} alt='Homem com ícones de desconto' className='w-full  block md:hidden'/>
-        <img src={'/imgs/discount-man-vertical.png'} alt='Homem com ícones de desconto' className='hidden md:block  w-full object-contain max-w-[752px]  text-left'/>
+      <div className='w-full lg:w-[50%]  lg:h-screen flex flex-col items-center  lg:items-center lg:justify-center pt-24'>
+        <img src='/imgs/acad-logo.png' alt='Acad Logotipo' className='w-[80%] max-w-[300px] lg:max-w-[80%] -mb-6 '/>
+        <div className='flex flex-col items-center lg:items-tart w-full lg:h-screen relative overflow-hidden'>
+          <img src={'/imgs/discount-man-vertical.png'} alt='Homem com ícones de desconto' className='hidden lg:block object-contain '/>
+          <img src={'/imgs/discount-man.png'} alt='Homem com ícones de desconto' className='max-w-[700px] block lg:hidden object-contain '/>
+          <div className='lg:hidden absolute bottom-0 left-0 w-full h-22 bg-white' style={{ clipPath: 'polygon(-50% 100%, 100% 0, 100% 100%, 0 100%)' }}></div>
+          <div className='lg:hidden absolute bottom-0 left-0 w-full h-22 bg-gradient-to-b from-black to-white opacity-20' style={{ clipPath: 'polygon(-50% 100%, 100% 0, 100% 100%, 0 100%)' }}></div>
+        </div>
       </div>
-      <div className='flex flex-col md:items-start justify-center md:justify-start md:text-start md:w-[50%] gap-4 mb-24 md:mb-0 px-4'>
+
+      <div className='flex flex-col lg:items-start justify-center lg:justify-start lg:text-start lg:w-[50%] gap-4 mb-24 lg:mb-0 px-4'>
         <Title tag='h1'>Descubra quanto sua academia pode economizar</Title>
         <Title tag='h2' className='text-acad-gray-dark mb-4'>sendo um associado ACAD Brasil!</Title>
-        <div className='w-full flex justify-center md:justify-start md:mt-12'>
+        <div className='w-full flex justify-center lg:justify-start lg:mt-12'>
           <Button onClick={() => setIsAppStarted(true)}>Calcular minha economia agora</Button>
         </div>
         <Link href='https://diagonal.ag/' target='_blank' className='mt-12 w-full flex justify-center'>
